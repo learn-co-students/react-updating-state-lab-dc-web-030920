8dba977e53676a4e85649b94a2464e65d22ed905
Dawit Gizaw <gizawdawit123.com>
2020-05-13 01:55:46 -0400

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,41 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+
+    changeBitrate = () => {
+        let obj = this.state.settings
+        obj.bitrate = 12
+        this.setState({settings: obj})
+    }
+
+    changeRes = () => {
+        let obj = this.state.settings
+        obj.video.resolution = '720p'
+        this.setState({settings: obj})
+    }
+    render(){   
+        return( 
+            <div>
+                <button className="bitrate" onClick={this.changeBitrate}></button>
+                <button className="resolution"onClick={this.changeRes}></button>
+            </div>
+        )
+    }
+
+
+}
